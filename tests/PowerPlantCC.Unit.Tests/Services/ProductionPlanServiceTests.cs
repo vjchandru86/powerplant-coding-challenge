f0939558831323ed246e70c0473ef14420b2dd9a
vjchandru86 <vijay.fibber@gmail.com>
2023-10-10 19:01:18 +0200

    refactored tests

--- tests/PowerPlantCC.Unit.Tests/Services/ProductionPlanServiceTests.cs
+++ tests/PowerPlantCC.Unit.Tests/Services/ProductionPlanServiceTests.cs
@@ -26,34 +26,15 @@ namespace PowerPlantCC.Unit.Tests.Services
             }
         }
 
-        [Fact]
-        public void CalculateProductionPlan_ForPayload1_ReturnsValidResponse()
+        [Theory]
+        [InlineData("payload1.json", "[\r\n  {\r\n    \"name\": \"windpark1\",\r\n    \"p\": 90\r\n  },\r\n  {\r\n    \"name\": \"windpark2\",\r\n    \"p\": 21.6\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig1\",\r\n    \"p\": 368.4\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig2\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"gasfiredsomewhatsmaller\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"tj1\",\r\n    \"p\": 0\r\n  }\r\n]")]
+        [InlineData("payload2.json", "[\r\n  {\r\n    \"name\": \"windpark1\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"windpark2\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig1\",\r\n    \"p\": 380\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig2\",\r\n    \"p\": 100\r\n  },\r\n  {\r\n    \"name\": \"gasfiredsomewhatsmaller\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"tj1\",\r\n    \"p\": 0\r\n  }\r\n]")]
+        public void CalculateProductionPlan_ForPayloads_ReturnsExpectedResponse(string fileName, string expectedResponse)
         {
             // Arrange
-            ProductionPlanRequest productionPlanRequest = JsonConvert.DeserializeObject<ProductionPlanRequest>(ReadFromJsonExamplePayload("payload1.json"))!;
+            ProductionPlanRequest productionPlanRequest = JsonConvert.DeserializeObject<ProductionPlanRequest>(ReadFromJsonExamplePayload(fileName))!;
             var ppService = new ProductionPlanService();
-            var responseString = "[\r\n  {\r\n    \"name\": \"windpark1\",\r\n    \"p\": 90\r\n  },\r\n  {\r\n    \"name\": \"windpark2\",\r\n    \"p\": 21.6\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig1\",\r\n    \"p\": 368.4\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig2\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"gasfiredsomewhatsmaller\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"tj1\",\r\n    \"p\": 0\r\n  }\r\n]";
-            ProductionPlanResponse[] productionPlanResponse = JsonConvert.DeserializeObject<ProductionPlanResponse[]>(responseString)!;
-
-            // Act
-            var response = ppService.CalculateProductionPlan(productionPlanRequest);
-
-            // Assert
-            for (int i = 0; i < response.Length; i++)
-            {
-                Assert.Equal(productionPlanResponse[i].Name, response[i].Name);
-                Assert.Equal(productionPlanResponse[i].P, response[i].P);
-            }
-        }
-
-        [Fact]
-        public void CalculateProductionPlan_ForPayload2_ReturnsValidResponse()
-        {
-            // Arrange
-            ProductionPlanRequest productionPlanRequest = JsonConvert.DeserializeObject<ProductionPlanRequest>(ReadFromJsonExamplePayload("payload2.json"))!;
-            var ppService = new ProductionPlanService();
-            var responseString = "[\r\n  {\r\n    \"name\": \"windpark1\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"windpark2\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig1\",\r\n    \"p\": 380\r\n  },\r\n  {\r\n    \"name\": \"gasfiredbig2\",\r\n    \"p\": 100\r\n  },\r\n  {\r\n    \"name\": \"gasfiredsomewhatsmaller\",\r\n    \"p\": 0\r\n  },\r\n  {\r\n    \"name\": \"tj1\",\r\n    \"p\": 0\r\n  }\r\n]";
-            ProductionPlanResponse[] productionPlanResponse = JsonConvert.DeserializeObject<ProductionPlanResponse[]>(responseString)!;
+            ProductionPlanResponse[] productionPlanResponse = JsonConvert.DeserializeObject<ProductionPlanResponse[]>(expectedResponse)!;
 
             // Act
             var response = ppService.CalculateProductionPlan(productionPlanRequest);
